09947cea0d6c012820dc37ed85aa1effc94db95d
Arthur Magalhães <160818381+arthurmagal@users.noreply.github.com>
2025-09-16 00:31:18 +0000

    15/09 - parte 2

--- projeto-inshe/src/main/java/entidades/Usuarios.java
+++ projeto-inshe/src/main/java/entidades/Usuarios.java
@@ -1,5 +1,17 @@
 package entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuarios {
 
+    @Id               
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+    public String nome;
+    public String email;
+    public String senha;
 }
